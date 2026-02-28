43bed2c6c5444c336cb8eb86c544504eb7b59745
primusr <primus.reyes@gmail.com>
2026-02-28 16:53:29 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -61,7 +61,7 @@ def configure_gemini(key):
         return None
     try:
         genai.configure(api_key=key)
-        return genai.GenerativeModel("models/gemini-2.0-flash")
+        return genai.GenerativeModel("models/gemini-3-flash-preview")
     except:
         return None
 
