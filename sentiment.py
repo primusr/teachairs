4f7eae5f6f8375f55292cbe40021b901126444fd
primusr <primus.reyes@gmail.com>
2026-02-28 16:49:02 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -53,8 +53,7 @@ st.title("📊 TeachAIRs: Student Feedback Analyzer with AI Recommendations")
 # ------------------------------
 # Gemini API (Optional)
 # ------------------------------
-api_key = "AIzaSyARU8Nk2VITEnVRr9EeyporBAmMetxzZII" 
-# st.text_input("🔑 Enter Gemini API Key (Optional)", type="password")
+api_key = st.text_input("🔑 Enter Gemini API Key (Optional)", type="password")
 
 @st.cache_resource
 def configure_gemini(key):
