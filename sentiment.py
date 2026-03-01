9168e04db176a072fcf6ef9103ca56a24beed7d8
primusr <primus.reyes@gmail.com>
2026-03-01 16:17:15 +0800

    dotenv added"

--- sentiment.py
+++ sentiment.py
@@ -9,11 +9,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-
-
-import os from dotenv 
-import load_dotenv
-
+import os
+from dotenv import load_dotenv
 import nltk
 nltk.download('punkt_tab')
 from nltk.corpus import stopwords
