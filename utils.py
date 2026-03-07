df96982dedb76200ab8377311326523a32a32971
primusr <primus.reyes@gmail.com>
2026-03-08 02:13:52 +0800

    Auto Update

--- utils.py
+++ utils.py
@@ -12,27 +12,22 @@ from nltk.stem import WordNetLemmatizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import google.generativeai as genai
 import streamlit as st
+
+
 st.markdown("""
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
 <style>
-html, body, [class*="css"] {
-  font-family: "Poppins", sans-serif;
-
+html, body, [class*="css"]  {
+    font-family: 'Poppins', sans-serif;
 }
 
-/* Sidebar */
-[data-testid="stSidebar"] {
-  font-family: "Poppins", sans-serif;
-
-}
-
-/* Buttons */
-button {
-  font-family: "Poppins", sans-serif;
-  
+h1, h2, h3, h4, h5, h6 {
+    font-family: 'Poppins', sans-serif;
 }
 </style>
 """, unsafe_allow_html=True)
+
 
 warnings.filterwarnings("ignore")
 
