c6d1da7e1faab0a7f77b3ac34b5658d939983dfe
primusr <primus.reyes@gmail.com>
2026-06-17 13:33:04 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -39,8 +39,7 @@ st.set_page_config(
     page_title="TeachAIRs",
     page_icon="🧊",
     layout="centered",
-    initial_sidebar_state="None",
-    
+    initial_sidebar_state="expanded",
     menu_items={
         "About": "Developed by Neo under the supervision of the Oracle. Watch this short video for a tutorial on how to use the app:  https://www.youtube.com/shorts/OvRlMiYURhM",
         "Get Help": "https://www.linkedin.com/in/unclebreaker/",
