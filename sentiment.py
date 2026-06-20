7381a4651add156edf9b17e0e56d31aa22b41534
primusr <primus.reyes@gmail.com>
2026-06-20 19:45:35 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -645,13 +645,15 @@ Distribution (Filipino Keywords):
                         f"Topic {topic_idx}"
                     )
                     with col:
-                       # st.subheader(f"Topic {topic_idx}: {ai_title}")
+                        st.markdown(f"Topic {topic_idx}: {ai_title}")
+                        st.markdown(f"Keywords: {words}")
+
                         wc = WordCloud(background_color="white", width=400, height=300)
                         wc.generate_from_frequencies(dict(words_probs))
                         fig, ax = plt.subplots(figsize=(6, 4))
                         ax.imshow(wc, interpolation="bilinear")
                         ax.axis("off")
-                        ax.set_title(f"Topic {topic_idx}: {ai_title} \n Top Keywords: {words}", fontsize=10)
+                        
                         st.pyplot(fig)
 
     else:
