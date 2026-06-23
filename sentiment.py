057d1baf80142246ffff56e30daadb939c3ae354
primusr <primus.reyes@gmail.com>
2026-06-24 06:32:47 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -16,7 +16,7 @@ from gensim.models import CoherenceModel
 from wordcloud import WordCloud
 import base64
 
-from reportlab.lib.pagesizes import letter, A4
+from reportlab.lib.pagesizes import letter, A4, landscape
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak, Table, TableStyle, Image
@@ -655,7 +655,7 @@ Here are 2-3 actionable teaching recommendations based on the topic \"{topic_lab
     def _build_comprehensive_pdf_report(filename):
         """Build a comprehensive PDF report with all analysis sections"""
         buffer = BytesIO()
-        doc = SimpleDocTemplate(buffer, pagesize=letter, topMargin=0.5*inch, bottomMargin=0.5*inch)
+        doc = SimpleDocTemplate(buffer, pagesize=landscape(letter), topMargin=0.5*inch, bottomMargin=0.5*inch)
         story = []
         styles = getSampleStyleSheet()
         
