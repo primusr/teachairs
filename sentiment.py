e53be32b9249ee05a36c06da8dff76fec5868981
primusr <primus.reyes@gmail.com>
2026-06-24 14:15:52 +0800

    Auto Update

--- sentiment.py
+++ sentiment.py
@@ -122,11 +122,11 @@ def build_report_pdf():
           body {{ font-family: Arial, sans-serif; padding: 5px; line-height: 1.0, font-size:8px; margin: 5px; }}
           h1, h2 {{ color: #1f4e79; page-break-after: avoid; break-after: avoid; }}
           table {{ border-collapse: collapse; width: 100%; max-width: 100%; font-size: 8px; table-layout: fixed; word-wrap: break-word; align: center; }}
-          th, td {{ border: 1px solid #ccc; padding: 4px; text-align: left; vertical-align: top; overflow-wrap: anywhere; align: center; }}
+          th, td {{ border: 1px solid #ccc; padding: 2px; text-align: center; vertical-align: center; overflow-wrap: anywhere; align: center;  }}
           img {{ max-width: 80%; height: auto; display: block; margin: 0 auto; }}
           div, p, li {{ overflow-wrap: anywhere; }}
           pre {{ white-space: pre-wrap; word-wrap: break-word; overflow-wrap: anywhere; }}
-          .report-section {{ page-break-inside: avoid; break-inside: avoid; margin-bottom: 12px; }}
+          .report-section {{ page-break-inside: avoid; break-inside: avoid; margin-bottom: 8px; }}
           .figure-section {{ page-break-inside: avoid; break-inside: avoid; }}
           .overall-recommendations {{ page-break-inside: avoid; break-inside: avoid; page-break-after: avoid; break-after: avoid; }}
         </style>
